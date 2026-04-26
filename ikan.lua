2a1d7b468c723ef86d97db10748e9c6f4c867f34
RDBULLX <nanang150603@gmail.com>
2026-04-26 08:40:17 +0700

    Update ikan.lua

--- ikan.lua
+++ ikan.lua
@@ -48,5 +48,6 @@ return {
     ["Iridesca"] = "https://raw.githubusercontent.com/bayubast/fish-data/main/Iridesca.png",
     ["Deepsea Monster Axolotl"] = "https://raw.githubusercontent.com/bayubast/fish-data/main/Deepsea%20Axolotl.png",
     ["Frostbite Leviathan"] = "https://raw.githubusercontent.com/bayubast/fish-data/main/Frosbite%20Leviathan.png",
-}
+    ["Blocky Lochness Monster"] = "https://raw.githubusercontent.com/bayubast/fish-data/main/Frosbite%20Leviathan.png",
+
 }
