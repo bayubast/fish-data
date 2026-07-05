744b22bdc41e481f3ec9b574083e1f842626ad4d
RDBULLX <nanang150603@gmail.com>
2026-07-05 08:32:49 +0700

    Add Trench Warden image URL to ikan.lua

--- ikan.lua
+++ ikan.lua
@@ -65,4 +65,5 @@ return {
     ["Elemental Tempestray"] = "https://raw.githubusercontent.com/bayubast/fish-data/main/Elemental%20Tempestray.png",
     ["Dark Megalodon"] = "https://raw.githubusercontent.com/bayubast/fish-data/main/Dark%20Megalodon.png",
     ["Caustic Maw"] = "https://raw.githubusercontent.com/bayubast/fish-data/main/Caustic%20Maw.png",
+    ["Trench Warden"] = "https://raw.githubusercontent.com/bayubast/fish-data/main/Trench%20Warden.png"
 }
